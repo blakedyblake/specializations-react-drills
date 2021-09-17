4bbe968e0d1a1c8fd767ca3308a6e88cfda8042c
старейшина <blakedyblake@gmail.com>
2021-09-17 11:37:23 -0600

    done with 4

--- app-4/src/App.js
+++ app-4/src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      userName: "",
+      userPassword:""
+    }
+    this.badSecurity = this.badSecurity.bind(this)
+  }
+ badSecurity(){
+   console.log('bad security')
+    let {userName, userPassword} = this.state
+    alert(`User Name: ${userName}  User Password:${userPassword}`)
+
+ }
+  render(){
+    //Note this use of map
+    
+    return (
+      <div className="App">
+          <fieldset>
+            <legend>sign in</legend>
+          <input id="userName" placeholder="username" onChange={event=>this.state.userName = event.target.value}/>
+          <input id="password" placeholder="password" onChange= {event=> this.state.userPassword = event.target.value}/>
+          <button id="login" type='submit' onClick={this.badSecurity}>Log in</button>
+          </fieldset>
+        
+          
+
+      </div>
+    );
+  }
+ 
 }
 
 export default App;
