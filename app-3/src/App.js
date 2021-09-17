7a4e85ed4b7cebe6e57c861e703b8a083b723619
старейшина <blakedyblake@gmail.com>
2021-09-17 11:20:18 -0600

    Array of JSX was a thing, now this is making sense...

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      list : ["Spagetti","Mushrooms", "Mariokart", "Dark Sun"],
+      userInput:"",
+      items: []
+    }
+    this.update = this.update.bind(this)
+  }
+ update(val){
+        this.setState({userInput: val})
+    }
+ 
+  render(){
+    //Note this use of map
+    let list = this.state.list.filter((e,i)=>{ //This includes it but not just if it starts with it
+      return e.includes(this.state.userInput)
+    }).map((e,i)=>{
+      return <h2 key= {i}>{e}</h2>
+    })   
+    return (
+      <div className="App">
+        <input id="filter" onChange={event=>this.update(event.target.value)} />
+          <div id='list'>
+              {list}
+          </div>
+      </div>
+    );
+  }
+ 
 }
 
 export default App;
