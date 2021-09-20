b5be5e3803f31e33451ee9ff6c6d440426291d7a
старейшина <blakedyblake@gmail.com>
2021-09-19 23:06:44 -0600

    Finished #7
    Much deliberation. React has many secrets...

--- app-7/src/App.js
+++ app-7/src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+class Todo extends Component {
+  render(){
+    return <p>{this.props.task}</p>
+  }
+}//This is the same...
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class List extends Component{
+  constructor(props){
+    super(props)
+  }
+  render(){
+    //arr props makes the list
+    let list = this.props.arr.map((e,i)=> {return <Todo key={i} task={e}/>})
+    return(
+      <div>{list}</div>
+    )
+  }
+}
+class NewTask extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      userInput:""
+    }
+    this.add= this.add.bind(this) //
+    this.changeVal = this.changeVal.bind(this)
+  }
+  add(){
+    this.props.add(this.state.userInput)//This links it, sending info to App allowing List to access it
+    this.setState({
+      // arr: [...this.state.arr, this.state.userInput],
+      userInput: "",
+
+  })
+  }
+  changeVal(val){
+    this.setState({
+      userInput: val
+    })
+  }
+  render(){
+    return (
+            <fieldset>
+              <legend>Add a task</legend>
+              <input value={this.state.userInput} placeholder="New Task" id="input" onChange= {event=> this.changeVal(event.target.value)}></input>
+              <button onClick= {this.add}>Add</button>
+            </fieldset>
+    )
+  }
 }
 
-export default App;
+class App extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+          list:[]
+        }        
+        this.add = this.add.bind(this)
+    }
+    add(task){
+      this.setState({list: [...this.state.list, task]})
+    }
+    render(){
+
+        return(
+          <div className="App">
+            <h1>My to-do List:</h1>
+            <NewTask add={this.add}/>
+            <br/>
+            <List arr={this.state.list}></List>
+            
+
+          </div>
+        )
+    }
+
+}
+
+export default App
